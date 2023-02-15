44fb4a909bb2a6398905e029a9eb4a3c172e9a18
Felixxxxl <81540099@qq.com>
2023-02-15 23:26:24 +0800

    Create unittest for OwnIngredients Serializer

--- ingredient/tests.py
+++ ingredient/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from ingredient.models import OwnIngredients
+from ingredient.serializers import OwnIngredientsSerializers
+from datetime import date
+
 
 class TestOwnIngredientsModel(TestCase):
 
@@ -14,4 +17,29 @@ class TestOwnIngredientsModel(TestCase):
 
     def test_owningredientsmodel(self):
         ingredient = OwnIngredients.objects.get(id = self.ingredient.id)
-        self.assertEqual(ingredient.amount,0.6)
+        self.assertEqual(ingredient.amount, 0.6)
+        ingredient = OwnIngredients.objects.get(id = self.ingredient.id)
+        self.assertEqual(ingredient.type,'food')
+        ingredient = OwnIngredients.objects.get(id = self.ingredient.id)
+        self.assertEqual(ingredient.name,'rice')
+        ingredient = OwnIngredients.objects.get(id = self.ingredient.id)
+        self.assertEqual(ingredient.unit,'pound')
+        ingredient = OwnIngredients.objects.get(id = self.ingredient.id)
+        self.assertEqual(ingredient.expiration_date,date(2025,6,1))
+
+
+class TestOwnIngredientsSerializers(TestCase):
+    def setUp(self):
+        self.ingredient = OwnIngredients.objects.create(
+            type = 'food',
+            name = 'rice',
+            amount = '0.6',
+            unit = 'pound',
+            expiration_date = '2025-06-01'
+        )
+
+        self.serializer = OwnIngredientsSerializers(instance=self.ingredient)
+
+    def test_serializer_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertCountEqual(data.keys(),['type','name','amount','unit','expiration_date'])
