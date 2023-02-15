d8b3033033409ee5db290460e9682ac0793cc9d8
Felixxxxl <81540099@qq.com>
2023-02-15 11:45:33 +0800

    create owningredients model

--- ingredient/models.py
+++ ingredient/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class OwnIngredients(models.Model):
+
+    unit_choices = [
+        ('kilogram', 'Kilogram'),
+        ('gram', 'Gram'),
+        ('ounce', 'Ounce'),
+        ('pound', 'Pound')
+    ]
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    amount = models.FloatField()
+    unit = models.CharField(max_length=12, choices=unit_choices) 
+    expiration_date = models.DateField()
+    
+    class Meta:
+        verbose_name = "own_ingredients"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+    
