42d2ae9324a0716e0910d7fbc612cc30845b1e86
Felixxxxl <81540099@qq.com>
2023-02-15 11:45:04 +0800

    Modify STATICFILES_DIR in setting.py

--- cherish/settings.py
+++ cherish/settings.py
@@ -110,9 +110,9 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"static")
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
 STATIC_URL = "static/"
 
 # Default primary key field type
