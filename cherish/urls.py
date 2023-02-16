242e8318f7af64080d684e3c8702d062187b9c3f
Felixxxxl <81540099@qq.com>
2023-02-16 12:25:33 +0800

    .

--- cherish/urls.py
+++ cherish/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ingredient import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ingredients/",views.IngredientsView.as_view())
 ]
