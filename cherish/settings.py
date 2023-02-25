be25b5cbadda18d34c53e7cd0f43affadb04fc5e
Felixxxxl <81540099@qq.com>
2023-02-25 15:38:42 +0800

    .Initial Recipe app

--- cherish/settings.py
+++ cherish/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "ingredient.apps.IngredientConfig",
+    "recipe.apps.RecipeConfig",
 ]
 
 MIDDLEWARE = [
--- recipe/apps.py
+++ recipe/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecipesConfig(AppConfig):
+class RecipeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "recipes"
+    name = "recipe"
