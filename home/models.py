41a82c1375ac024b6b6963828faae390d16ac2df
Felixxxxl <81540099@qq.com>
2023-03-05 01:59:29 +0800

    .

--- home/models.py
+++ home/models.py
@@ -3,9 +3,6 @@ from ingredient.models import OwnIngredient
 
 
 # Create your models here.
-
-
-
 class IngredientStatusLog(models.Model):
     log_id = models.AutoField(primary_key=True)
     ingredient = models.ForeignKey(OwnIngredient, on_delete=models.DO_NOTHING,related_name='log')
